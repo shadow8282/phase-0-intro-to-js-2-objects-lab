7a750d5c09291e1e2e05dc1f455684ad4bba43ee
Cayden <Caydenburr@gmail.com>
2023-02-13 17:06:24 -0700

    coplete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = { name:"joe", streetAddress: "Mama's House" };
+
+function updateEmployeeWithKeyAndValue( employee, key, value) 
+{
+    const objClone = { ...employee};
+    objClone[key] = value;
+    return objClone;
+    
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value)
+{
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key)
+{   
+    const objClone = { ...employee};
+    delete objClone[key];
+    return objClone;
+};
+
+function destructivelyDeleteFromEmployeeByKey (employee, key)
+{
+   delete employee[key];
+    return employee;
+};
